047c300b7bc2ae03688583b63c895cc4f29d01ce
6dayspizza <55084543+6dayspizza@users.noreply.github.com>
2024-09-17 09:47:12 +0000

    setup vue

--- backend/app.js
+++ backend/app.js
@@ -9,17 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //templating enginge
-app.engine('.hbs', engine({ extname: '.hbs' }));
-app.set('view engine', '.hbs');
-app.set('views', path.join(__dirname, 'views'));
-
-// middleware
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../frontend/helloyou-vue/dist')));
 
 // routes
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+    res.sendFile(path.join(__dirname, '../frontend/helloyou-vue/dist/index.html'));
+  });
 
 // server start
 app.listen(PORT, () => {
